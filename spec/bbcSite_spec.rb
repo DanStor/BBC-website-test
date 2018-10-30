b2a7b381bd4a8838d30fe5eb9404886064ba7d5c
Daniel Storey <dstorey@spartaglobal.com>
2018-10-30 11:53:50 +0000

    added test questions

--- spec/bbcSite_spec.rb
+++ spec/bbcSite_spec.rb
@@ -2,6 +2,29 @@ require 'spec_helper'
 
 describe "testing the bbc site" do
   context "testing the bbc login functionality" do
-    
+
+    it "should allow the user to navigate to the login page" do
+
+    end
+
+    it "should allow the user to login with the correct details" do
+
+    end
+
+    it "should display an error for blank username" do
+
+    end
+
+    it "should display an error for an incorrect password" do
+
+    end
+
+    it "should display an error for a short password" do
+
+    end
+
+    it "should get rekt" do
+
+    end
   end
 end
